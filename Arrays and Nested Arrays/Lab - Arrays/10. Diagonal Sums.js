0c3435694476ea22b40c2b5808c340541fc8c42d
Petar Ivanov <123456789petar45@gmail.com>
2023-02-06 22:26:59 +0200

    Update

--- Arrays and Nested Arrays/Lab - Arrays/10. Diagonal Sums.js
+++ Arrays and Nested Arrays/Lab - Arrays/10. Diagonal Sums.js
@@ -1,13 +1,16 @@
-function diagonalSums(input) {
-
-    let leftSum = input[0];
-    let rightSum = input.length -1;
-    console.log(input[1][rightSum]);
-
-
-
+function diagonalSums(matrix) {
     
+    let mainDiagonalSum = 0;
+    let secondaryDiagonalSum = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        mainDiagonalSum += Number(matrix[i][i]);
+        secondaryDiagonalSum += Number(matrix[i][matrix.length - 1 - i]);
+    }
+    return [mainDiagonalSum, secondaryDiagonalSum].join(` `);
 }
-diagonalSums([[3, 5, 17],
-              [-1, 7, 14],
-              [1, -8, 89]])
+
+let result = diagonalSums([[3, 5, 17],
+[-1, 7, 14],
+[1, -8, 89]])
+
+console.log(result);
