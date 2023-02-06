b687b959a6621685854b4db3c28bfab5ab24b3af
Petar Ivanov <123456789petar45@gmail.com>
2023-02-06 17:39:33 +0200

    Update

---
+++ Arrays and Nested Arrays/Lab - Arrays/3. Sum First Last.js
@@ -0,0 +1,11 @@
+function sumFirstLast(input) {
+
+    let firstEl = Number(input.shift());
+    let lastEl = Number(input.pop());
+
+    return firstEl + lastEl;
+
+}
+
+let result = sumFirstLast(['10', '20', '30', `40`, `50`])
+console.log(result);
