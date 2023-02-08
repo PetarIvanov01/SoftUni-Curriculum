12ae8657b0db3cf798b810536c9341dcff32ee34
Petar Ivanov <123456789petar45@gmail.com>
2023-02-08 19:28:35 +0200

    Update 6. From JSON to HTML Table.js

--- Objects and Composition/Lab - Objects & Composition/6. From JSON to HTML Table.js
+++ Objects and Composition/Lab - Objects & Composition/6. From JSON to HTML Table.js
@@ -5,7 +5,6 @@ function jsonToHtml(input) {
     let columnNames = Object.keys(obj[0])
     let values = obj.map(el => Object.values(el))
 
-
     let result = `<table>\n`;
 
     appendHeaders(columnNames)
@@ -19,19 +18,12 @@ function jsonToHtml(input) {
             for (const command of value) {
                 
             result += `<td>${escape(command)}</td>`
-            //<table>
-            //<tr><th>Name</th><th>Score</th><th>Grade</th></tr>
-            // <tr><td>Pesho</td><td>4</td><td>8</td></tr>
-            //<tr><td>Gosho</td><td>5</td><td>8</td></tr>
-            // <tr><td>Angel</td><td>5.5</td><td>10</td></tr>
-            //</table>
-
+            
             }
             result += `</tr>`
             result += `\n`
         }
     }
-
     function appendHeaders(columnNames) {
         result += `   <tr>`
         for (let columName of columnNames) {
@@ -41,12 +33,16 @@ function jsonToHtml(input) {
         }
         result += `</tr>\n`
     }
+
     function escape(value) {
-        return value.toString().replace('<', '&lt;').replace('>', '&gt;')
+        return value.toString()
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
     }
     console.log(result);
-
-
 }
 jsonToHtml(`[{"Name":"Stamat",
 
