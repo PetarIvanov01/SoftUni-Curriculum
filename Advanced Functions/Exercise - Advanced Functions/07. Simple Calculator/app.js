ae3d20c5a5ff9da4a52d2e01e4a30b9f5ec2584a
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-02-28 20:36:18 +0200

    Update app.js

--- Advanced Functions/Exercise - Advanced Functions/07. Simple Calculator/app.js
+++ Advanced Functions/Exercise - Advanced Functions/07. Simple Calculator/app.js
@@ -1,6 +1,36 @@
 function calculator() {
-    // TODO:
+
+    let num1 = undefined;
+    let num2 = undefined;
+    let result = undefined;
+
+    return {
+        init,
+        add,
+        subtract
+    }
+
+    function init(par1, par2, par3) {
+
+        num1 = document.querySelector(`${par1}`);
+        num2 = document.querySelector(`${par2}`);
+        result = document.querySelector(`${par3}`);
+
+    }
+    function add() {
+
+        result.value = Number(num1.value) + Number(num2.value);
+
+    }
+    function subtract() {
+
+        result.value = Number(num1.value) - Number(num2.value);
+
+    }
 }
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
+
 
 
 
