b8618d72dc8a0222400954a7e5e0a8280b8dd865
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-03-04 13:33:08 +0200

    Update 01. Sub Sum.js

--- Unit Testing Configuration/01. Sub Sum.js
+++ Unit Testing Configuration/01. Sub Sum.js
@@ -19,13 +19,3 @@ function sum(arr, start, end) {
 
     return sum
 }
-
-console.log(sum(1, 2, 3));
-console.log('------');
-console.log(sum([10, 20, 30, 40, 50, 60], 3, 300))
-console.log('------');
-console.log(sum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
-console.log('------');
-console.log(sum([10, 'twenty', 30, 40], 0, 2));
-console.log('------');
-console.log(sum('text', 0, 2));
