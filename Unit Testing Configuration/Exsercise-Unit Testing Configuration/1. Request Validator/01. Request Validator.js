b359a14b1ca72d8e132f27c5fc0c781adc8f3e02
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-03-06 20:08:15 +0200

    Create 01. Request Validator.js

---
+++ Unit Testing Configuration/Exsercise-Unit Testing Configuration/1. Request Validator/01. Request Validator.js
@@ -0,0 +1,35 @@
+function validator(input) {
+
+    const method = input.method;
+    const uri = input.uri;
+    const version = input.version;
+    const message = input.message;
+
+    if (!['GET', 'POST', 'DELETE', 'CONNECT'].includes(method) || method === undefined) {
+        // 'Invalid request header: Invalid Method'
+        throw new Error(`Invalid request header: Invalid Method`);
+    }
+    let uriMatcher = /(^[\w.]+$)|\*/
+    //let uriPattern = /[^A-Za-z0-9\.\*]+/;
+    if (!uriMatcher.test(uri) || uri == '' || uri === undefined) {
+        throw new Error('Invalid request header: Invalid URI');
+    }
+    if (!['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'].includes(version) || version === undefined) {
+        throw new Error('Invalid request header: Invalid Version')
+    }
+    let messageMatcher = /^[^<>\\&'"]*$/
+    if (!messageMatcher.test(message) || message == undefined) {
+        throw new Error('Invalid request header: Invalid Message')
+    }
+
+    return input;
+
+}
+let obj = {
+    method: 'POST',
+    version: 'HTTP/2.0',
+    message: 'rm -rf /*'
+};
+validator(obj)
+
+//Invalid request header: Invalid Version
