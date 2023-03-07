1a900b810f282c905ffd9ad2b0268737318ad292
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-03-07 19:54:44 +0200

    Update app.js

--- Unit Testing Configuration/Exsercise-Unit Testing Configuration/6.Dynamic-Validation/app.js
+++ Unit Testing Configuration/Exsercise-Unit Testing Configuration/6.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    let emailField = document.getElementById('email');
+    let reg = /[a-z]+@[a-z]+\.[a-z]+/gm;
+
+    emailField.addEventListener('change', () => {
+
+        if (!reg.test(emailField.textContent)) {
+            emailField.style === 'none';
+        }
+        emailField.style === 'error';
+
+    })
 }
