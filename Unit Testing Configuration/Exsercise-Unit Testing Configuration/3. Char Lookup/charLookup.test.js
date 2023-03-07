e6ccd4ab109fc10e0ac5ee56f8e3c52552c73600
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-03-07 11:02:25 +0200

    Update

---
+++ Unit Testing Configuration/Exsercise-Unit Testing Configuration/3. Char Lookup/charLookup.test.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+const lookupChar = require('./charLookup');
+
+
+describe('Test for lookupChar function', () => {
+    //incorect parametars
+    it('returns undefined if the first parametar is not a string', () => {
+        expect(lookupChar(1, 1)).to.be.undefined;
+        expect(lookupChar([], 1)).to.be.undefined;
+        expect(lookupChar({}, 1)).to.be.undefined;
+    })
+    it('returns undefined if the second parametar is not a number', () => {
+        expect(lookupChar('ivan', 's')).to.be.undefined;
+        expect(lookupChar('ivan', [])).to.be.undefined;
+        expect(lookupChar('ivan', {})).to.be.undefined;
+    })
+    it('returns "Incorrect index" if the second parametar is biiger than string length', () => {
+        expect(lookupChar('ivan',10)).to.equal('Incorrect index')
+    })
+    it('returns "Incorrect index" if the second parametar is lower than 0', () => {
+        expect(lookupChar('ivan',-1)).to.equal('Incorrect index')
+    })
+    it('returns "Incorrect index" if the second parametar is decimal', () => {
+        expect(lookupChar('ivan',0.5)).to.be.undefined
+    })
+    //corect parametars
+    it('returns undefined if the first parametar is not a number', () => {
+        expect(lookupChar('ivan',2)).to.equal('a')
+    })
+})
+
+
+
+
