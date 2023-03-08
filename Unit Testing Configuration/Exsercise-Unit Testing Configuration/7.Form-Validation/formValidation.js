6884cae43503f553379a14c44c656dcc324eb3c3
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-03-08 08:50:51 +0200

    Update formValidation.js

--- Unit Testing Configuration/Exsercise-Unit Testing Configuration/7.Form-Validation/formValidation.js
+++ Unit Testing Configuration/Exsercise-Unit Testing Configuration/7.Form-Validation/formValidation.js
@@ -1,114 +1,79 @@
 function validate() {
-    // TODO
-    let username = document.getElementById('username');
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    let confPass = document.getElementById('confirm-password');
+
     let company = document.getElementById('company');
-    let companyInfo = document.getElementById('companyInfo');
-    let companyNumber = document.getElementById('companyNumber');
     let buttonSubm = document.getElementById('submit');
 
-    let validFlag = false;
+    company.addEventListener('change', (e) => {
+        let companyInfo = document.getElementById('companyInfo');
+        let isChecked = company.checked;
+        if (isChecked) {
+            companyInfo.style.display = 'block';
+        }
+        else {
+            companyInfo.style.display = 'none';
+        }
+    })
 
-    let valid = document.getElementById('valid');
-
-    buttonSubm.addEventListener('click', submit)
-
-    function submit(event) {
+    buttonSubm.addEventListener('click', (event) => {
         event.preventDefault()
 
-        let validators = {
-            usernameVal,
-            emailVal,
-            passwordVal,
-            companyVal,
+        let validOut = [];
+        let userName = document.querySelector("#username");
+        let email = document.querySelector("#email");
+        let passWord = document.querySelector("#password");
+        let confirmPass = document.querySelector("#confirm-password");
+        let checkBox = document.querySelector("#company");
+        let userTest = /^[A-Za-z0-9]{3,20}$/;
+        let emailTest = /^[^@.]+@[^@]*\.[^@]*$/;
+        let passTest = /^[\w]{5,15}$/;
+
+        if (!userTest.test(userName.value)) {
+            userName.style.borderColor = "red";
+            validOut.push(false);
+        } else {
+            userName.style.borderColor = "";
+            validOut.push(true);
         }
 
+        if (!emailTest.test(email.value)) {
+            email.style.borderColor = "red";
+            validOut.push(false);
+        } else {
+            email.style.borderColor = "";
+            validOut.push(true);
+        }
 
-        usernameVal();
-        emailVal();
-        passwordVal();
-        companyVal()
+        if (
+            passWord.value === confirmPass.value &&
+            passTest.test(confirmPass.value) &&
+            passTest.exec(passWord.value)
+        ) {
+            confirmPass.style.borderColor = "";
+            passWord.style.borderColor = "";
+            validOut.push(true);
+        } else {
+            confirmPass.style.borderColor = "red";
+            passWord.style.borderColor = "red";
+            validOut.push(false);
+        }
 
-        if (validFlag) {
+        if (checkBox.checked) {
+            let company = document.querySelector("#companyNumber");
+            if (company.value < 1000 || company.value > 9999) {
+                company.style.borderColor = "red";
+                validOut.push(false);
+            } else {
+                company.style.borderColor = "";
+                validOut.push(true);
+            }
+        }
+
+        let valid = document.getElementById('valid');
+        if (!validOut.includes(false)) {
             valid.style.display = 'block';
         }
         else {
             valid.style.display = 'none';
         }
-    }
-
-    function usernameVal() {
-        let reg = /^[A-z0-9]{3,20}$/m
-        let value = username.value;
-        if (!reg.test(value)) {
-            username.style.borderColor = 'red';
-            validFlag = false
-        }
-        else {
-            username.style.border = '';
-            validFlag = true
-        }
-    }
-
-    function emailVal() {
-        let reg = /[a-z]+@[a-z]+\.{1,}[a-z]+/m
-        let value = email.value;
-        if (!reg.test(value)) {
-            email.style.borderColor = 'red';
-            validFlag = false
-        }
-        else {
-            email.style.border = '';
-            validFlag = true
-        }
-    }
-
-    function passwordVal() {
-        let reg = /^[\w]{5,15}$/m
-
-        let value = password.value;
-        let confValue = confPass.value;
-
-        if (!reg.test(value) || !reg.test(confValue) || value !== confValue) {
-
-            password.style.borderColor = 'red';
-            confPass.style.borderColor = 'red'
-            validFlag = false
-        }
-        else {
-            password.style.border = '';
-            confPass.style.border = '';
-            validFlag = true
-        }
-    }
-
-    function companyVal() {
-        let isChecked = company.checked;
-        if (isChecked) {
-            companyInfo.style.display = 'block';
-
-        }
-
-        else {
-            companyInfo.style.display = 'none';
-
-        }
-
-        companyNumber.addEventListener('change', (event) => {
-            let number = Number(event.target.value);
-            if (number > 1000 && number < 9999) {
-                companyNumber.style.border = ''
-                validFlag = true
-            }
-            else {
-                companyNumber.style.borderColor = 'red';
-                validFlag = false
-            }
-        })
-    }
-
-
-
+    })
 }
