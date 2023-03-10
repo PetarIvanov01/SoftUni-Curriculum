c80598c70b3564ab3feb1b8e7286c71b7f7beb96
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-03-10 16:34:10 +0200

    Update 01.Person.js

--- Classes/Lab - Classes/01.Person.js
+++ Classes/Lab - Classes/01.Person.js
@@ -11,3 +11,16 @@ class Person {
 }
 let person = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
 console.log(person.toString());
+
+// This is a example of the problem solved with function
+// function Person(firstName, lastName, age, email) {
+
+//     this.firstName = firstName,
+//         this.lastName = lastName,
+//         this.age = age,
+//         this.email = email
+
+//     Person.prototype.toString = function () {
+//         return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+//     };
+// }
