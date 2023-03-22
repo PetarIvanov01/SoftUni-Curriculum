d2726a4795069b6677d9fc9f280453238d13b6ac
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-03-22 16:58:24 +0200

    Update app.js

--- Js Applications/02.Asynchronous Programming/Exercise/01.Bus-Stop/app.js
+++ Js Applications/02.Asynchronous Programming/Exercise/01.Bus-Stop/app.js
@@ -1,42 +1,26 @@
 function getInfo() {
-    const btn = document.getElementById('submit');
-    const field = document.getElementById('stopId').value
 
-    const url = `http://localhost:3030/jsonstore/bus/businfo/${field}`;
-    const parrent = document.getElementById('stopName');
-    const busesStops = document.getElementById('buses');
+    const stopId = document.getElementById("stopId").value;
+    const stopNameEl = document.getElementById("stopName");
+    const busesEl = document.getElementById("buses");
 
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error')
-            }
-            return response.json();
-        })
-        .then(value => {
-            appendElements(value);
+    // Clear previous result
+    stopNameEl.textContent = "";
+    busesEl.innerHTML = "";
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+        .then(response => response.json())
+        .then(data => {
+            stopNameEl.textContent = data.name;
+            Object.entries(data.buses).forEach(([busId, time]) => {
+                const li = document.createElement("li");
+                li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                busesEl.appendChild(li);
+            });
         })
         .catch(error => {
-            catchErrors(error);
-        })
+            console.error(error);
+            stopNameEl.textContent = "Error";
+        });
 
-    function appendElements(value) {
-
-        busesStops.innerHTML = ''
-        let stName = value.name;
-        let busTime = value.buses;
-
-        parrent.textContent = stName;
-
-        for (const key in busTime) {
-            let li = document.createElement('li');
-            li.textContent = `Bus ${key} arrives in ${busTime[key]} minute`;
-            busesStops.appendChild(li)
-        }
-    }
-
-    function catchErrors(error) {
-        parrent.textContent = 'Error';
-        busesStops.innerHTML = ''
-    }
 }
