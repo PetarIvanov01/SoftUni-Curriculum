b6709511f33537ba520f2c466f64fcc18f9d401d
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-04-04 20:17:52 +0300

    Update app.js

--- Js Applications/02.Asynchronous Programming/Exercise/06.Blog/app.js
+++ Js Applications/02.Asynchronous Programming/Exercise/06.Blog/app.js
@@ -6,41 +6,67 @@ function attachEvents() {
     const postBody = document.querySelector("#post-body");
     const postComments = document.querySelector("#post-comments");
 
-    loadPostsBtn.addEventListener("click", loadPosts);
-    viewPostBtn.addEventListener("click", viewPost);
 
-    async function loadPosts() {
+    try {
 
-        const res = await fetch("http://localhost:3030/jsonstore/blog/posts");
-        const data = await res.json();
-       
-        Object.values(data).map(el => postsSelect.appendChild(createEL('option', el.id, el.title)))
+        loadPostsBtn.addEventListener("click", loadPosts);
+        viewPostBtn.addEventListener("click", viewPost);
 
-    }
+        async function loadPosts() {
 
-    async function viewPost() {
+            const res = await fetch("http://localhost:3030/jsonstore/blog/posts");
 
-        const postId = postsSelect.value;
-        const commentsRes = await fetch("http://localhost:3030/jsonstore/blog/comments");
-        const commentsData = await commentsRes.json();
-
-        postComments.innerHTML = "";
-
-        for (const comment of Object.values(commentsData)) {
-
-            if (comment.postId === postId) {
-
-                postComments.appendChild(createEL('li', undefined, comment.text));
-
+            if (!res.ok) {
+                throw new Error('Failed response');
             }
+            const data = await res.json();
+
+            Object.values(data).map(el => postsSelect.appendChild(createEL('option', el.id, el.title)))
 
         }
 
-        const postRes = await fetch(`http://localhost:3030/jsonstore/blog/posts/${postId}`);
-        const postData = await postRes.json();
+        async function viewPost() {
 
-        postTitle.textContent = postData.title;
-        postBody.textContent = postData.body;
+            const postId = postsSelect.value;
+            const commentsRes = await fetch("http://localhost:3030/jsonstore/blog/comments");
+
+            if (!commentsRes.ok) {
+                throw new Error('Failed response');
+            }
+
+            const commentsData = await commentsRes.json();
+
+            postComments.innerHTML = "";
+
+            for (const comment of Object.values(commentsData)) {
+
+                if (comment.postId === postId) {
+
+                    let id = comment.id
+                    let li = createEL('li', undefined, comment.text);
+                    li.setAttribute('id', id)
+                    postComments.appendChild(li);
+
+                }
+
+            }
+
+            const postRes = await fetch(`http://localhost:3030/jsonstore/blog/posts/${postId}`);
+
+            if (!postRes.ok) {
+                throw new Error('Failed response');
+            }
+
+            const postData = await postRes.json();
+
+            postTitle.textContent = postData.title;
+            postBody.textContent = postData.body;
+
+        }
+    }
+    catch (error) {
+
+        console.log(error.message);
 
     }
 
