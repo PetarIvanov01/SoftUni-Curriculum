1ec4849dfec0bbad7bb3142e41a2e6d70d5b3945
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-04-07 18:14:35 +0300

    Update
    Broke down the task into separate modules.

--- Js Applications/03.Single Page Applications/Lab Single Page Application/01.Calendar/main.js
+++ Js Applications/03.Single Page Applications/Lab Single Page Application/01.Calendar/main.js
@@ -1,20 +1,13 @@
-const sections = Array.from(document.querySelectorAll('section'));
-const mainSection = sections[0];
+import { hideAllElements } from './router.js';
+import { displayMonth } from './displayPages.js';
+import { sections, mainSection } from './data.js';
 
-hideAllElements()
-function hideAllElements() {
+hideAllElements();
 
-    mainSection.style.display = 'block'
-    for (let i = 1; i < sections.length; i++) {
-        sections[i].style.display = 'none'
-    }
-
-}
 mainSection.addEventListener('click', (event) => {
 
-    hideAllElements()
-    let target = event.target
-
+    hideAllElements();
+    let target = event.target;
 
     if (target.tagName == 'DIV') {
 
@@ -24,50 +17,14 @@ mainSection.addEventListener('click', (event) => {
         mainSection.style.display = 'none';
 
         const currentYear = currentSection.querySelector('.calendar');
-        currentYear.addEventListener('click', displayMonth.bind(null, year, currentSection))
+
+        currentYear.addEventListener('click', (e) => {
+
+            displayMonth(e, currentSection, year);
+
+        })
     }
-})
+});
 
-function displayMonth(year, currentSection, event) {
 
-    if (event.target.tagName == 'CAPTION') {
-        hideAllElements()
-    }
-    else if (event.target.tagName == 'DIV') {
 
-        let month = event.target.textContent;
-
-        const monthsToDays = {
-            'Jan': 1,
-            'Feb': 2,
-            'Mar': 3,
-            'Apr': 4,
-            'May': 5,
-            'Jun': 6,
-            'Jul': 7,
-            'Aug': 8,
-            'Sept': 9,
-            'Oct': 10,
-            'Nov': 11,
-            'Dec': 12,
-        };
-
-        const currentMonth = monthsToDays[month];
-
-        let daysPage = sections.filter(sec => sec.id == `month-${year}-${currentMonth}`)[0];
-        daysPage.style.display = 'block';
-        currentSection.style.display = 'none';
-
-        daysPage.addEventListener('click', returnsToMonths.bind(null, currentSection))
-    }
-}
-
-function returnsToMonths(currentSection, event) {
-    
-    if (event.target.tagName == 'CAPTION') {
-
-        event.currentTarget.style.display = 'none';
-        currentSection.style.display = 'block'
-
-    }
-}
