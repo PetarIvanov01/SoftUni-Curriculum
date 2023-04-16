77f6ef88a4bf814f998113c7cd316c81e8e7255e
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-04-16 21:19:34 +0300

    Finished

--- Js Applications/03.Single Page Applications/Exersice/02.Movies/src/register.js
+++ Js Applications/03.Single Page Applications/Exersice/02.Movies/src/register.js
@@ -1,9 +1,59 @@
-import { showView } from "./util.js";
+import { homeView } from "./home.js";
+import { showView, updateNav } from "./util.js";
+
 
 const registerSec = document.querySelector('#form-sign-up');
+const form = registerSec.querySelector('form');
 
 export function registerView() {
 
     showView(registerSec)
 
+}
+
+form.addEventListener('submit', onSubmit)
+
+async function onSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const rePass = formData.get('repeatPassword');
+    await sendRegReq(email, password, rePass);
+
+    form.reset();
+    homeView();
+    updateNav();
+
+}
+
+async function sendRegReq(email, password, rePass) {
+    console.log(email,password,rePass);
+    try {
+        if (email == '' || password == '' || rePass == '') {
+            throw new Error('All fields are required!')
+        }
+        if (password !== rePass) {
+            throw new Error('Wrong re-password!')
+        }
+        const res = await fetch('http://localhost:3030/users/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password, rePass })
+        });
+        if (!res.ok) {
+            const error = await res.json();
+            throw new Error(error.message);
+        }
+
+        const user = await res.json();
+        sessionStorage.setItem('user', JSON.stringify(user));
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
 }
