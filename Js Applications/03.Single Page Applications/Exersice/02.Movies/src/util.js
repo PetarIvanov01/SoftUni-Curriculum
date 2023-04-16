45331b061662c3cd8d959474646aa9e5679a0142
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-04-16 21:19:40 +0300

    Update util.js

--- Js Applications/03.Single Page Applications/Exersice/02.Movies/src/util.js
+++ Js Applications/03.Single Page Applications/Exersice/02.Movies/src/util.js
@@ -1,7 +1,5 @@
 const allView = document.querySelectorAll('.view-section')
 
-// showView('#home-page')
-
 function hideAllView() {
     allView.forEach(v => v.style.display = 'none')
 
@@ -12,4 +10,26 @@ export function showView(section) {
     section.style.display = 'block';
 }
 
+export function spinner() {
+    const element = document.createElement('p');
+    element.innerHTML = 'Loading &hellip;';
+
+    return element;
+}
+
+export function updateNav() {
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const msgContaier = document.querySelector('.nav-link');
+
+    if (user) {
+        document.querySelectorAll('.user').forEach(el => el.style.display = 'inline-block')
+        document.querySelectorAll('.guest').forEach(el => el.style.display = 'none')
+        msgContaier.textContent = `Welcome, ${user.email}`;
+    }
+    else {
+        document.querySelectorAll('.user').forEach(el => el.style.display = 'none')
+        document.querySelectorAll('.guest').forEach(el => el.style.display = 'inline-block')
+        msgContaier.textContent = '';
+    }
+}
 
