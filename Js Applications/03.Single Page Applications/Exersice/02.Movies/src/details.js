02d2e0005d27697df6b997ffa3d2b9c9208ea8af
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-04-16 21:19:21 +0300

    Update

--- Js Applications/03.Single Page Applications/Exersice/02.Movies/src/details.js
+++ Js Applications/03.Single Page Applications/Exersice/02.Movies/src/details.js
@@ -1,6 +1,7 @@
 import { showView } from "./util.js";
 
 const detailsSec = document.querySelector('#movie-example');
+
 export function detailsView() {
 
     showView(detailsSec)
--- Js Applications/03.Single Page Applications/Exersice/02.Movies/src/home.js
+++ Js Applications/03.Single Page Applications/Exersice/02.Movies/src/home.js
@@ -1,8 +1,56 @@
-import { showView } from "./util.js";
+import { detailsView } from "./details.js";
+import { showView, spinner, updateNav } from "./util.js";
 
-const homeSec = document.querySelector('#home-page');
+const section = document.querySelector('#home-page');
+const movieSection = section.querySelector('.card-deck.d-flex.justify-content-center');
+
 export function homeView() {
 
-    showView(homeSec)
+    showView(section);
+    displayMovies();
+    updateNav();
 
+}
+
+movieSection.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.tagName == 'BUTTON') {
+        const id = event.target.id;
+        detailsView(id);
+    }
+
+})
+
+export async function getMovies() {
+    const res = await fetch('http://localhost:3030/data/movies')
+    const data = await res.json()
+
+    return data
+}
+
+async function displayMovies() {
+
+    movieSection.replaceChildren(spinner())
+    const data = await getMovies();
+    movieSection.replaceChildren(...data.map(createMovie))
+
+}
+
+function createMovie(movie) {
+    let element = document.createElement('div')
+    element.className = 'card mb-4';
+
+    element.innerHTML = `
+    <img class="card-img-top" src="${movie.img}"
+            alt="Card image cap" width="400">
+        <div class="card-body">
+            <h4 class="card-title">${movie.title}</h4>
+        </div>
+        <div class="card-footer">
+            <a href="#/details/6lOxMFSMkML09wux6sAF">
+                <button type="button" id="${movie._id}" class="btn btn-info">Details</button>
+            </a>
+        </div>
+    `
+    return element
 }
