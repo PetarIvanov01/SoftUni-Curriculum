4d7f407a080088f1ed9c4b92e8103d7bba014bdb
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-07 16:51:40 +0300

    Check if user already exist

--- JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/services/authentication.js
+++ JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/services/authentication.js
@@ -5,18 +5,28 @@ const bcrypt = require('bcrypt')
 
 async function registerUser(username, password) {
 
-    const user = new User({
-        username,
-        password: await hashedFunc(password)
-    })
+    try {
+        if (await User.findOne({ username: username })) {
+            
+            throw new Error('User exist!');
+        }
 
-    const savedUser = await user.save();
-    const userId = savedUser._id;
+        const user = new User({
+            username,
+            password: await hashedFunc(password)
+        })
 
-    // Generate JWT with user ID
-    const token = jwt.sign({ userId }, 'secretKey');
+        const savedUser = await user.save();
+        const userId = savedUser._id;
 
-    return token
+        const token = jwt.sign({ userId }, 'secretKey');
+        return token
+
+    } catch (error) {
+
+        throw error
+    }
+
 }
 async function compareHash(password, hash) {
     return bcrypt.compare(password, hash);
