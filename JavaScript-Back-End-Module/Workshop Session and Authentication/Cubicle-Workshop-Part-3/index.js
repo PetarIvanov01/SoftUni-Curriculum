645a43002a710e7c935d40ed910c1bed2067e198
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-07 16:52:13 +0300

    Adding login,register controllers and also auth middleware

--- JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/index.js
+++ JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-const url = 'mongodb://127.0.0.1:27017/testdb';
+const url = 'mongodb://127.0.0.1:27017/cubicalDb';
 
 start();
 async function start() {
@@ -10,19 +12,22 @@ async function start() {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
-    
+
     console.log('The database is connected');
 
     const hbs = require('express-handlebars').create({
         extname: '.hbs'
     });
 
-    const authMiddleware = require('./controllers/middlewares/authControler')
+    const authMiddleware = require('./controllers/middlewares/authControler');
     const homeControler = require('./controllers/homeControler');
     const createControler = require('./controllers/createControler');
     const detailsControler = require('./controllers/detailsControler');
     const accessoryControler = require('./controllers/accessoryControler');
+    const loginControler = require('./controllers/AuthenticationControllers/login')
+    const registerControler = require('./controllers/AuthenticationControllers/register')
     const notFoundPage = require('./controllers/notFound');
+
 
     const app = express();
 
@@ -32,11 +37,19 @@ async function start() {
     app.use(express.urlencoded({ extended: true }))
     app.use('/static', express.static('static'));
 
-    app.use(authMiddleware)
+    app.use(session({
+        secret: 'your-secret-key',
+        resave: false,
+        saveUninitialized: true,
+    }));
+    app.use(cookieParser());
+    app.use(authMiddleware);
     app.use(homeControler);
-    app.use('/create',createControler);
+    app.use(loginControler);
+    app.use(registerControler);
+    app.use('/create', createControler);
     app.use('/details', detailsControler);
-    app.use('/accessory',accessoryControler)
+    app.use('/accessory', accessoryControler);
     app.use(notFoundPage);
 
     app.listen(3000, () => console.log('The server is running on port 3000.'));
