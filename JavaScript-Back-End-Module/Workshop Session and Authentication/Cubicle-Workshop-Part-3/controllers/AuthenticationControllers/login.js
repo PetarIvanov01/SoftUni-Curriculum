90ad0f507a5c48c46f61e676db6ff69a23518528
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-08 19:48:37 +0300

    Implement user login functionality with error handling

--- JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/controllers/AuthenticationControllers/login.js
+++ JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/controllers/AuthenticationControllers/login.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
+const { hashedFunc, loginUser } = require('../../services/authentication')
 
 router.get('/login', (req, res) => {
-    console.log('login');
-    res.render('user/login');
-    
+
+    const isExist = req.session.userErr;
+    req.session.userErr = null
+    res.render('user/login', {
+        error: isExist
+    });
+})
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        if (username.trim() == '' && password.trim() == '') {
+            throw new Error('All fields are required!')
+        }
+        const token = await loginUser(username, password);
+
+        res.cookie('jwt', token);
+        res.redirect('/')
+
+    } catch (error) {
+
+        req.session.userErr = error.message
+        res.redirect('/login');
+    }
+
+
 })
 
 
