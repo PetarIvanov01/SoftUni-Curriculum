beaa2adf9c655a1386b4b2bc5b20db98a020c4ab
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-08 19:47:07 +0300

    Update

--- JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/controllers/middlewares/authControler.js
+++ JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/controllers/middlewares/authControler.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken')
 
+const key = 'secretKey'
 const authentication = (req, res, next) => {
     const token = req.cookies.jwt || undefined;
     if (token) {
         try {
-            // Verify and decode the JWT token
-            const decodedToken = jwt.verify(token, 'secretKey');
-            // Assign the decoded token to req.user
+
+            const decodedToken = jwt.verify(token, key);
             req.user = decodedToken;
 
         } catch (error) {
-            // Token is invalid or expired
-            res.cookie('jwt', '', { maxAge: 0 })
+
+            res.clearCookie('jwt');
+            // res.cookie('jwt', '', { maxAge: 0 })
             return res.redirect('/login')
         }
     }
