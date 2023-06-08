9f6af149040d21230175a9f4de640359541a93af
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-08 19:46:54 +0300

    Authentication for login user

--- JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/services/authentication.js
+++ JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/services/authentication.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 async function registerUser(username, password) {
 
     try {
         if (await User.findOne({ username: username })) {
-            
+
             throw new Error('User exist!');
         }
 
@@ -28,6 +28,27 @@ async function registerUser(username, password) {
     }
 
 }
+async function loginUser(username, password) {
+
+    const user = await User.findOne({ username: { $regex: new RegExp('^' + username + '$', 'i') } });
+
+    try {
+        if (user == undefined || compareHash(password, user.password) == false) {
+            throw new Error('Invalid username or password!');
+        }
+        const userId = user._id;
+
+        const token = jwt.sign({ userId }, 'secretKey');
+        return token
+
+    } catch (error) {
+        
+        throw error
+    }
+}
+
+
+
 async function compareHash(password, hash) {
     return bcrypt.compare(password, hash);
 }
@@ -37,4 +58,4 @@ async function hashedFunc(password) {
     return bcrypt.hash(password, 10);
 }
 
-module.exports = { registerUser, compareHash }
+module.exports = { registerUser, compareHash, loginUser }
