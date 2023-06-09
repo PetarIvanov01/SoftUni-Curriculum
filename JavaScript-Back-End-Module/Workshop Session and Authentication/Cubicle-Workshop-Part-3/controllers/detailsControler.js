322c1845aba1763f28ef63ea47ad7ccd76ac9527
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-09 17:23:07 +0300

    Add delete and edit functionality for items

--- JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/controllers/detailsControler.js
+++ JavaScript-Back-End-Module/Workshop Session and Authentication/Cubicle-Workshop-Part-3/controllers/detailsControler.js
@@ -1,4 +1,4 @@
-const { getById, getPopulatedAcc } = require('../services/requests');
+const { getById, getPopulatedAcc, deleteById, updateById } = require('../services/requests');
 
 const router = require('express').Router();
 
@@ -9,6 +9,11 @@ router.get('/:id', async (req, res) => {
     const user = req.user;
     const item = await getById(id);
     const cube = await getPopulatedAcc(id);
+    let isOwner;
+
+    if (user) {
+        isOwner = item.creatorId == user.userId;
+    }
 
     if (item == undefined) {
         res.render('404');
@@ -17,6 +22,7 @@ router.get('/:id', async (req, res) => {
 
         res.render('details', {
             isUser: user,
+            isOwner,
             title: 'Details Page',
             cube: item,
             accessories: cube.accessories
@@ -24,6 +30,49 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+//Show Delete
+router.get('/delete/:id', async (req, res) => {
 
+    const id = req.params.id;
+    const item = await getById(id);
 
+    res.render('item/delete', {
+        title: 'Details Page',
+        cube: item,
+    });
+
+})
+
+//Show Edit
+router.get('/edit/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const item = await getById(id);
+
+    res.render('item/edit', {
+        title: 'Details Page',
+        cube: item,
+    });
+})
+
+//Post Delete
+router.post('/delete/:id', async (req, res) => {
+
+    const id = req.params.id
+    await deleteById(id);
+
+    res.redirect('/');
+
+})
+
+//Post Edit
+router.post('/edit/:id', async (req, res) => {
+
+    const currentId = req.params.id
+    const body = req.body;
+
+    const id = await updateById(currentId, body);
+    res.redirect('/details/' + id);
+
+})
 module.exports = router;
