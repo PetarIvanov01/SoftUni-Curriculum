0afb603c68b99c2d468f9958bb535f4e04f050aa
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-10 17:00:36 +0300

    Catalog controler done

--- JavaScript-Advanced-Module/Js Applications/Exams-applicaton/JS App 2 April 2022/src/views/catalog.js
+++ JavaScript-Advanced-Module/Js Applications/Exams-applicaton/JS App 2 April 2022/src/views/catalog.js
@@ -1,29 +1,40 @@
-import { html} from '../util/lib.js'
+import { getCatalog } from '../api/apiPets.js';
+import { html, until } from '../util/lib.js'
 
 export function catalogControler(ctx) {
 
-    ctx.render(template());
+    ctx.render(template(getItems()));
+
 }
-const template = () => html`
+const template = (promise) => html`
 <!--Dashboard-->
 <section id="dashboard">
-        <h2 class="dashboard-title">Services for every animal</h2>
-        <div class="animals-dashboard">
-            <div class="animals-board">
-                <article class="service-img">
-                    <img class="animal-image-cover" src="./images/cat2.jpg">
-                </article>
-                <h2 class="name">Athena</h2>
-                <h3 class="breed">American Curl</h3>
-                <div class="action">
-                    <a class="btn" href="/details/1">Details</a>
-                </div>
-            </div>
-
-            <!--If there is no pets in dashboard-->
-            <div>
-                <p class="no-pets">No pets in dashboard</p>
-            </div>
+    ${until(promise,html`<div>Loading...</div>`)}
         </div>
     </section>
 `
+const itemTemplate = (item) => html`
+<div class="animals-board">
+                <article class="service-img">
+                    <img class="animal-image-cover" src="${item.image}">
+                </article>
+                <h2 class="name">${item.name}</h2>
+                <h3 class="breed">${item.breed}</h3>
+                <div class="action">
+                    <a class="btn" href="/details/${item._id}">Details</a>
+                </div>
+`
+async function getItems() {
+
+    const items = await getCatalog();
+
+    return items.length ? html`
+    <h2 class="dashboard-title">Services for every animal</h2>
+        <div class="animals-dashboard">
+            ${items.map(itemTemplate)}
+            </div>
+    `:
+        html`<div>
+                <p class="no-pets">No pets in dashboard</p>
+            </div>`
+}
