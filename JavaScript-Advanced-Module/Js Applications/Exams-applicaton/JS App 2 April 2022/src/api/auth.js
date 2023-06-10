879d83601436bf3e33971c92b2bde6ad78696926
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-10 16:37:05 +0300

    Implement user authentication functions and endpoint configuration

---
+++ JavaScript-Advanced-Module/Js Applications/Exams-applicaton/JS App 2 April 2022/src/api/auth.js
@@ -0,0 +1,44 @@
+import { post, get } from "../api/apiRequests.js";
+import { clearUserData, setUserData } from "../../src/api/user.js";
+
+
+const endpoints = {
+    register: '/users/register',
+    login: '/users/login',
+    logout: '/users/logout',
+};
+
+export async function login(email, password) {
+
+    const user = await post(endpoints.login, { email, password });
+    alert('You are logged in!');
+    setDataToLocaleSt(user)
+}
+
+export async function register(email, password) {
+
+    const user = await post(endpoints.register, { email, password });
+    alert('You are registerd!');
+    setDataToLocaleSt(user)
+}
+export async function logout(ctx) {
+
+    await get(endpoints.logout);
+    alert('Logged out!')
+    clearUserData();
+
+    ctx.updateNav();
+    ctx.page.redirect('/')
+}
+
+function setDataToLocaleSt(user) {
+
+    const userData = {
+        email: user.email,
+        password: user.password,
+        token: user.accessToken,
+        id: user._id
+    }
+    setUserData(userData);
+
+}
