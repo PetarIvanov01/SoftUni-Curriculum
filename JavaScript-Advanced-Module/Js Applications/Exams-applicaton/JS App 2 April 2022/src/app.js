7c8988d2266d6acb7cc3ffdbbbb3faf83de50fcb
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-10 15:55:29 +0300

    Add routing configuration using page.js

---
+++ JavaScript-Advanced-Module/Js Applications/Exams-applicaton/JS App 2 April 2022/src/app.js
@@ -0,0 +1,23 @@
+import page from '../node_modules/page/page.mjs'
+import { setUser, withRenderMiddleware } from './util/middleware.js';
+import { catalogControler } from './views/catalog.js';
+import { createControler } from './views/create.js';
+import { detailsControler } from './views/details.js';
+import { editControler } from './views/edit.js';
+import { homeControler, } from './views/home.js';
+import { loginControler } from './views/login.js';
+import { registerControler } from './views/register.js';
+
+
+page(setUser);
+page(withRenderMiddleware);
+
+page('/', homeControler);
+page('/catalog',catalogControler);
+page('/create',createControler);
+page('/details/:id',detailsControler);
+page('/edit/:id',editControler);
+page('/login',loginControler);
+page('/register',registerControler);
+
+page.start();
