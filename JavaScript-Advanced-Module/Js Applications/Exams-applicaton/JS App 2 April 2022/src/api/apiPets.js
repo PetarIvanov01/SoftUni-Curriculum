b217457a6814ed103a10882316be962575cb4bd4
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-11 15:31:37 +0300

    Update

--- JavaScript-Advanced-Module/Js Applications/Exams-applicaton/JS App 2 April 2022/src/api/apiPets.js
+++ JavaScript-Advanced-Module/Js Applications/Exams-applicaton/JS App 2 April 2022/src/api/apiPets.js
@@ -6,37 +6,36 @@ const endpoints = {
     create: 'data/pets',
     donation: 'data/donation',
     details: (id) => `data/pets/${id}`,
+    edit: (id) => `data/pets/${id}`,
     delete: (id) => `data/pets/${id}`,
     total: (petId) => `data/donation?where=petId%3D%22${petId}%22&distinct=_ownerId&count`,
     own: (petId, userId) =>
-      `data/donation?where=petId%3D%22${petId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
-  };
-  
+        `data/donation?where=petId%3D%22${petId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
+};
 
+//CRUD
 export async function getCatalog() {
     return await get(endpoints.catalog);
 }
 export async function createPet(data) {
     return await post(endpoints.create, data);
 }
+export async function editPet(id, data) {
+    return await put(endpoints.edit(id), data)
+}
 export async function getDetails(id) {
     return await get(endpoints.details(id))
 }
-// export async function getLikes() {
-//     return await get(endpoints.likes);
-// }
-// export async function updateDetails(id, data) {
-//     return await put(endpoints.update + id, data)
-// }
-// export async function deleteItem(id) {
-//     return await del(endpoints.delete(id))
-// }
-// export async function getOwnLikes(albumId, userId) {
-//     return await get(endpoints.own(albumId, userId))
-// }
-// export async function getAllLikes(id) {
-//     return await get(endpoints.total(id))
-// }
-// export async function isLike(data) {
-//     return await post(endpoints.likes, data)
-// }
+export async function deleteItem(id) {
+    return await del(endpoints.delete(id))
+}
+//Donation logic requests
+export async function sendDonations(data) {
+    return await post(endpoints.donation, data);
+}
+export async function getOwnDonations(petId, userId) {
+    return await get(endpoints.own(petId, userId))
+}
+export async function getByIdDonations(id) {
+    return await get(endpoints.total(id))
+}
