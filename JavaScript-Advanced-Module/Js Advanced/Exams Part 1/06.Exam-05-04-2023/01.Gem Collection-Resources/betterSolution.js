daeab4a7622bd2e15d7790223e9ac49bd7670c0d
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-17 00:58:41 +0300

    Done!

--- JavaScript-Advanced-Module/Js Advanced/Exams Part 1/06.Exam-05-04-2023/01.Gem Collection-Resources/betterSolution.js
+++ JavaScript-Advanced-Module/Js Advanced/Exams Part 1/06.Exam-05-04-2023/01.Gem Collection-Resources/betterSolution.js
@@ -6,41 +6,34 @@ function solve() {
     const form = document.querySelector('form');
 
     const [preview, collection] = Array.from(document.querySelectorAll('#side-wrapper div ul'));
-    const fields = Array.from(document.querySelectorAll('.inner-wrap input'));
-    const field5 = document.querySelector('.inner-wrap select');
-
     let saveBtn;
     let editBtn;
     let cancelBtn;
 
     addButton.addEventListener('click', (event) => {
         event.preventDefault();
-        let copyFields;
 
-        const [field1, field2, field3, field4] = fields
-        copyFields = {
-            field1: field1.value,
-            field2: field2.value,
-            field3: field3.value,
-            field4: field4.value,
-            field5: field5.value
-        }
+        const formData = new FormData(form);
 
-        if (fields.some(el => el.value == '') || field5.value == '') {
+        const transformedData = Object.entries(Object.fromEntries(formData)).reduce((obj, [key, value]) => {
+            obj[key] = value;
+            return obj;
+        }, {});
+        
+        if (Object.values(transformedData).some(el => el.trim() == '')) {
             return;
         }
 
-
         addButton.disabled = true;
         let li = document.createElement('li')
-        li.setAttribute('class', 'gem-info')
+        li.setAttribute('class','gem-info')
         li.innerHTML = `
         <article>
-            <h4>${field1.value}</h4>
-            <p>Color: ${field2.value}</p>
-            <p>Carats: ${field3.value}</p>
-            <p>Price: ${field4.value}$</p>
-            <p>Type: ${field5.value}</p>
+            <h4>${transformedData.field1}</h4>
+            <p>Color: ${transformedData.field2}</p>
+            <p>Carats: ${transformedData.field3}$</p>
+            <p>Price: ${transformedData.field4}$</p>
+            <p>Type: ${transformedData.field5}</p>
         </article>
         <button class="save-btn">Save to Collection</button>
         <button class="edit-btn">Edit Information</button>
@@ -48,41 +41,44 @@ function solve() {
 
         preview.appendChild(li)
 
-        form.reset();
         // Get a reference to the buttons
         saveBtn = document.querySelector('.save-btn');
         editBtn = document.querySelector('.edit-btn');
         cancelBtn = document.querySelector('.cancel-btn');
 
         // Add click event listener to the "Save to Collection" button
-        saveBtn.addEventListener('click', saveFunc.bind(null, copyFields));
+        saveBtn.addEventListener('click', saveFunc.bind(null, transformedData));
 
         // Add click event listener to the "Edit Information" button
-        editBtn.addEventListener('click', editFunc.bind(null, copyFields));
+        editBtn.addEventListener('click', editFunc.bind(null, transformedData));
 
         // Add click event listener to the "Cancel" button
         cancelBtn.addEventListener('click', cancelFunc);
+        form.reset();
     })
 
     function saveFunc(data) {
-        let li = document.createElement('li')
-        li.innerHTML += `
-        <p class="collection-item">${data.field1} - Color: ${data.field2}/ Carats: ${data.field3}/ Price: ${data.field4}$/ Type: ${data.field5}</p>`
-        collection.appendChild(li);
-
         addButton.disabled = false
         preview.innerHTML = '';
+        collection.innerHTML += `
+        <li>
+            <p class="collection-item">${data.field1} - Color: ${data.field2}/ 
+                Carats: ${data.field3}/ Price: ${data.field4}$/ Type: ${data.field5} 
+            </p>
+        </li>
+        `
     }
     function editFunc(data) {
+
         const childrens = Array.from(form.querySelector('div .inner-wrap').children);
         childrens.forEach((ch, i) => ch.value = Object.values(data)[i]);
+        preview.innerHTML = '';
 
         addButton.disabled = false
-        preview.innerHTML = '';
     }
     function cancelFunc(event) {
-        preview.innerHTML = '';
         addButton.disabled = false;
+        preview.innerHTML = '';
     }
 
 }
