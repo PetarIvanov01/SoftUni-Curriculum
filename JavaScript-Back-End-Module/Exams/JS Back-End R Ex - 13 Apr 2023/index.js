3082621d1dcb851cd8812167fa6194972b049f32
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-18 22:28:39 +0300

    Config the session and cookieParser

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/index.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = require('./src/controllers/main/routers');
+const database = require('./src/database/config');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
@@ -9,15 +12,22 @@ start();
 async function start() {
 
     const app = express();
+    await database();
 
     app.engine('.hbs', hbs.engine);
     app.set('view engine', '.hbs');
+    app.use(express.urlencoded({ extended: true }));
 
     app.use('/static', express.static(path.join(__dirname, 'src', 'static')));
 
     app.set('views', path.join(__dirname, 'src', 'views'));
-    app.use(express.urlencoded({ extended: true }))
 
+    app.use(session({
+        secret: 'your-secret-key',
+        resave: false,
+        saveUninitialized: true,
+    }));
+    app.use(cookieParser());
     //Setting all routes
     app.use(router);
 
