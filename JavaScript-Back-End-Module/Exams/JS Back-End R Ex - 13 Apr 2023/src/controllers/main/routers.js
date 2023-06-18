4f0138160453fd02da62398a897e527e684d796e
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-18 15:10:12 +0300

    Configured router with all controllers

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/controllers/main/routers.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/controllers/main/routers.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
-const homeController = require('../homeController')
-
+const homeController = require('../homeController');
+const catalogController = require('../catalogController');
+const createController = require('../createController');
+const authController = require('../user/authController');
+const searchController = require('../searchController');
+const errorController = require('../errorController');
+const detailsController = require('../detailsController');
 
 router.use(homeController);
+router.use('/catalog', catalogController);
+router.use('/create',  createController);
+router.use('/auth', authController);
+router.use('/search', searchController);
+router.use('/details', detailsController);
+router.use('*', errorController);
 
 
 
