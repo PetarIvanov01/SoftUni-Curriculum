341e50dae6ec5ed5e4328595cc18b8a98445d318
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-18 14:29:05 +0300

    Initial setup of Express server with Handlebars view engine and static file serving

---
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = require('./src/controllers/main/routers');
+const path = require('path');
+const hbs = require('express-handlebars').create({
+    extname: '.hbs'
+});
+
+start();
+async function start() {
+
+    const app = express();
+
+    app.engine('.hbs', hbs.engine);
+    app.set('view engine', '.hbs');
+
+    app.use('/static', express.static(path.join(__dirname, 'src', 'static')));
+
+    app.set('views', path.join(__dirname, 'src', 'views'));
+    app.use(express.urlencoded({ extended: true }))
+
+    //Setting all routes
+    app.use(router);
+
+    app.listen(3000, () => {
+        console.log("listening on http://localhost:3000");
+    })
+}
+
+
+// app.use('/static', express.static(path.join(__dirname, 'src', 'static')));
+
+// app.set('views', path.join(__dirname, 'src', 'views'));
+// app.use(express.urlencoded({ extended: true }))
