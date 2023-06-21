dd91c60c76f4b55f35fbd2be91a7430e55781e36
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-21 13:38:33 +0300

    Refactor Express.js configuration and handlebars setup
    This commit refactors the Express.js configuration and handlebars setup for improved organization and readability.
    
    Changes include:
    - Updated import paths for Express.js and handlebars
    - Moved session middleware to a separate file for better modularity
    - Updated views and static files directories using path.join for better path resolution
    - Simplified handlebars engine creation and configuration
    
    The commit aims to enhance the Express.js configuration and handlebars setup for a more maintainable and organized codebase.

---
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/config/expressConfig.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const handlabars = require('express-handlebars');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('../controllers/middlewares/navigation');
+
+module.exports = (app) => {
+
+    const hbs = handlabars.create({
+        extname: '.hbs'
+    })
+    app.engine('.hbs', hbs.engine);
+    app.set('view engine', '.hbs');
+    app.set('views', path.join(__dirname,'..', 'views'));
+    
+    app.use('/static', express.static(path.join(__dirname,'..', 'static')));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(session(app));
+
+}
