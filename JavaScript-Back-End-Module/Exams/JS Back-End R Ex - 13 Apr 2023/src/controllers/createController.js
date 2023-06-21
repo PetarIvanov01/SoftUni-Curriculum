0a11e7f07353f40a2cf44fd4a7d98058ad2cb97e
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-21 15:44:18 +0300

    Implement game creation route and logic

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/controllers/createController.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/controllers/createController.js
@@ -1,8 +1,41 @@
+const { createItem } = require('../services/gameService');
+const { parseError } = require('../util/parser');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
 
     res.render('create');
+})
+
+router.post('/', async (req, res) => {
+
+    const { platform, name, image, price, genre, description } = req.body;
+
+    try {
+        if (Object.values(req.body).some(v => v == '')) {
+            throw new Error('All fields are required!')
+        }
+        const id = res.locals.user._id
+        await createItem(id, { platform, name, image, price: Number(price), genre, description });
+
+        res.redirect('/catalog');
+    } catch (error) {
+        const errors = parseError(error);
+        res.render('create', {
+            title: 'Create Page',
+            errors,
+            body: {
+                platform: req.body.platform,
+                name: req.body.name,
+                image: req.body.image,
+                price: req.body.price,
+                genre: req.body.genre,
+                description: req.body.description
+            }
+        })
+    }
+
 })
 
 
