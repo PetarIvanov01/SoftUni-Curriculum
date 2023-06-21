b9d87c46e6c07d92ac75a085e74b977167f65b30
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-21 15:21:09 +0300

    Adding trimBody middleware

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/config/expressConfig.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/config/expressConfig.js
@@ -3,6 +3,7 @@ const handlabars = require('express-handlebars');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const session = require('../controllers/middlewares/navigation');
+const trimBody = require('../controllers/middlewares/trimBody');
 
 module.exports = (app) => {
 
@@ -11,11 +12,12 @@ module.exports = (app) => {
     })
     app.engine('.hbs', hbs.engine);
     app.set('view engine', '.hbs');
-    app.set('views', path.join(__dirname,'..', 'views'));
-    
-    app.use('/static', express.static(path.join(__dirname,'..', 'static')));
+    app.set('views', path.join(__dirname, '..', 'views'));
+
+    app.use('/static', express.static(path.join(__dirname, '..', 'static')));
     app.use(express.urlencoded({ extended: true }));
+    app.use(trimBody())
     app.use(cookieParser());
-    app.use(session(app));
+    app.use(session());
 
 }
