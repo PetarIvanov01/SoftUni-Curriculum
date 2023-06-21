cd79e062667a3cccc21880cfa76d3e57858baf24
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-21 15:46:42 +0300

    Update user model with additional validations and indexes

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/models/User.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/models/User.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
 
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+    username: { type: String, required: true, minlength: [5, 'Username must be at least 5 characters long'] },
+    email: { type: String, required: true, minlength: [10, 'Email must be at least 10 characters long'] },
+    password: { type: String, required: true, minlength: [4, 'Password must be at least 4 characters long'] }
 
 })
+User.index({ username: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
+User.index({ email: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
 
 module.exports = mongoose.model('User', User)
