587689cf7f2ccacc2bd6f87e27503229cd47dc73
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-23 15:22:55 +0300

    Update search functionality and add middleware

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/controllers/searchController.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/controllers/searchController.js
@@ -1,10 +1,30 @@
+const { searchGames, getAll } = require('../services/gameService');
+const { hasUser } = require('./middlewares/guards');
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', hasUser(), async (req, res) => {
+    const games = await getAll();
 
-    res.render('search');
+    res.render('search', {
+        games
+    });
 })
 
+router.post('/', async (req, res) => {
 
+    const { name, platform } = req.body;
+    let games;
+    if (name == '' && platform == '') {
+        games = await getAll();
+    }
+    else {
+        games = await searchGames(name, platform);
+    }
+
+    res.render('search', {
+        games
+    })
+})
 
 module.exports = router
