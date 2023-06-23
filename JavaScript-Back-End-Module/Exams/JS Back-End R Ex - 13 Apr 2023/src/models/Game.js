127b4c2a4b5838b7e7cb08259dc61fc7c38b030b
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-23 15:25:51 +0300

    Update

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/models/Game.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex - 13 Apr 2023/src/models/Game.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const gameSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: [4, 'Username must be at least 4 characters long'] },
     image: {
-        type: String, required: true, validate: {
+        type: String, required: true,
+        validate: {
             validator: function (value) {
                 return /^https?:\/\//.test(value);
             },
@@ -11,8 +12,8 @@ const gameSchema = new mongoose.Schema({
         }
     },
     price: { type: Number, required: true, min: [0, 'Price must be a positive number!'] },
-    description: { type: String, required: true, minlength: [10, 'Username must be at least 10 characters long'] },
-    genre: { type: String, required: true, minlength: [2, 'Username must be at least 2 characters long'] },
+    description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
+    genre: { type: String, required: true, minlength: [2, 'Genre must be at least 2 characters long'] },
     platform: {
         type: String, required: true, enum: ["PC", "Nintendo", "PS4", "PS5", "XBOX"]
     },
