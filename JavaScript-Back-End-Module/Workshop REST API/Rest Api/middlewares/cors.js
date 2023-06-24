6aabc549467a5de294fd69f4742d30f23dad9b11
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-24 20:34:28 +0300

    Update

--- JavaScript-Back-End-Module/Workshop REST API/Rest Api/middlewares/cors.js
+++ JavaScript-Back-End-Module/Workshop REST API/Rest Api/middlewares/cors.js
@@ -1,8 +1,9 @@
 module.exports = () => {
     return (req, res, next) => {
+
         res.setHeader('Access-Control-Allow-Origin', '*')
-        res.setHeader('Access-Control-Allow-Methods', 'HEAD,OPTIONS,GET,POST,PUT,DELETE')
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization')
 
         next();
     }
