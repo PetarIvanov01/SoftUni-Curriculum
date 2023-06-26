3da226bd325262fcb46a930b754fa793e4498d91
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-26 10:13:50 +0300

    Adding two middlewares

--- JavaScript-Back-End-Module/Workshop REST API/Rest Api/config/express.js
+++ JavaScript-Back-End-Module/Workshop REST API/Rest Api/config/express.js
@@ -1,9 +1,13 @@
 const cors = require('../middlewares/cors');
 const express = require('express');
+const trimBody = require('../middlewares/trim');
+const session = require('../middlewares/session');
 
 module.exports = (app) =>  {
 
     app.use(cors());
     app.use(express.json());
+    app.use(trimBody());
+    app.use(session());
 
 }
