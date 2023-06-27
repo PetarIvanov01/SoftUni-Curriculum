6f85eabc5fa673bf4779fc41479269bf9297a0ad
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-27 12:53:04 +0300

    Update

--- JavaScript-Back-End-Module/Workshop REST API/Rest Api/controllers/itemController.js
+++ JavaScript-Back-End-Module/Workshop REST API/Rest Api/controllers/itemController.js
@@ -1,10 +1,89 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
+const errParser = require('../util/parser');
+const { create, getAll, getById, getOwnerItems, deleteItem, editItem } = require('../services/furnitureService');
 
-router.get('/catalog', (req, res) => {
+router.get('/catalog', async (req, res) => {
 
-    res.json([]);
-    
-    
+    try {
+        let items;
+        const params = req.query.where
+        if (params) {
+            const ownerId = params.split('=')[1];
+            items = await getOwnerItems(JSON.parse(ownerId));
+        }
+        else {
+            items = await getAll();
+        }
+        res.status(200).json(items);
+
+    } catch (error) {
+        res.status(500).json('Internal Server Error')
+    }
 })
+
+router.post('/catalog',
+    body('img').
+        isURL({ protocols: 'http' }).
+        withMessage('URL is invalid'),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (errors.length < 0) {
+                throw errors;
+            }
+            const userId = req.user._id;
+            await create(req.body, userId)
+
+            res.status(204).end();
+        } catch (error) {
+            const message = errParser(error);
+            res.status(400).json({ message })
+        }
+    })
+
+router.get('/catalog/:id', async (req, res) => {
+    try {
+
+        const id = req.params.id
+        const item = await getById(id);
+
+        res.status(200).json(item);
+
+    } catch (error) {
+        res.status(500).json('Internal Server Error')
+    }
+})
+
+router.delete('/catalog/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await deleteItem(id);
+
+        res.status(204).end();
+    } catch (error) {
+        res.status(500).json('Internal Server Error')
+    }
+})
+
+router.put('/catalog/:id',
+    body('img').
+        isURL({ protocols: 'http' }).
+        withMessage('URL is invalid'),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (errors.length < 0) {
+                throw errors;
+            }
+            const id = req.params.id;
+            await editItem(id, req.body);
+
+            res.status(204).end();
+        } catch (error) {
+            const message = errParser(error);
+            res.status(400).json({ message })
+        }
+    })
 
 module.exports = router
