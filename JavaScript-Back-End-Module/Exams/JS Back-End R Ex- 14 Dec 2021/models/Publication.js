9d7fce04bdc258ac573a0b7811260b1b222a36db
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-28 09:02:38 +0300

    Publication model

---
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex- 14 Dec 2021/models/Publication.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose')
+
+const pubSchema = new Schema({
+    title: { type: String, required: true },
+    technique: { type: String, required: true },
+    picture: { type: String, required: true },
+    certificate: { type: String, required: true },
+    _author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    _shared: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+
+})
+
+
+const Publication = model('Publication', pubSchema);
+module.exports = Publication;
