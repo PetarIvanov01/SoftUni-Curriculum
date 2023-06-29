554217f71fb171bd3eb5f73dfbe5b8e9408d9623
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-06-29 16:14:24 +0300

    Update

--- JavaScript-Back-End-Module/Exams/JS Back-End R Ex- 14 Dec 2021/config/routers.js
+++ JavaScript-Back-End-Module/Exams/JS Back-End R Ex- 14 Dec 2021/config/routers.js
@@ -2,11 +2,13 @@ const homeController = require("../controllers/homeController");
 const authController = require('../controllers/authController');
 const galleryController = require('../controllers/galleryController');
 
+const { hasUser } = require('../middlewares/guards')
+
 module.exports = (app) => {
 
     app.get('/favicon.ico', (req, res) => res.status(204))
     app.use('/', homeController);
     app.use('/user', authController);
-    app.use('/gallery', galleryController)
+    app.use('/gallery', hasUser, galleryController)
 
 }
