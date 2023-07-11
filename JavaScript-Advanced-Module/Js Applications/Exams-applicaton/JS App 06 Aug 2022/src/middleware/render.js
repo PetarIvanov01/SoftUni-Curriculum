fda8cbf9ab2cba07c99f9530e105edbca2b54b7b
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-11 11:57:46 +0300

    Implemented rendering middleware and user navigation update
    This commit introduces a rendering middleware and updates the user navigation display based on the user's authentication status. The following changes have been made:
    
    - Added `renderMiddleware` function to handle rendering of content using Lit HTML.
    - Implemented `updateNav` function to update the user navigation display based on the user's authentication status.
    - Updated the navigation display in the HTML based on the user's authentication status.

---
+++ JavaScript-Advanced-Module/Js Applications/Exams-applicaton/JS App 06 Aug 2022/src/middleware/render.js
@@ -0,0 +1,29 @@
+import { render, html } from "../../node_modules/lit-html/lit-html.js"
+import { until } from "../../node_modules/lit-html/directives/until.js"
+import { getUserData } from "../auth/storage.js";
+
+const body = document.querySelector('main');
+
+export const renderMiddlware = (ctx, next) => {
+
+    ctx.render = (content) => render(content, body)
+    ctx.update = updateNav;
+
+    updateNav();
+    next();
+}
+
+const updateNav = () => {
+    const user = getUserData();
+    
+    if (user) {
+        document.querySelector('.user').style.display = 'block';
+        document.querySelector('.guest').style.display = 'none';
+    }
+    else {
+        document.querySelector('.guest').style.display = 'block';
+        document.querySelector('.user').style.display = 'none';
+    }
+}
+
+export { html, until }
