561d8c39151a84ea28906c27ab48ae9f820ea861
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-19 21:36:07 +0300

    Commit message: "Add page routing and controllers"
    Explanation: This commit introduces page routing and connects it with corresponding controllers to handle different views and functionalities.

---
+++ JavaScript-Advanced-Module/Js Applications/07.Exercise Modular Applications/src/app.js
@@ -0,0 +1,36 @@
+import page from "../node_modules/page/page.mjs";
+import { catalogController } from "./controllers/page/catalog.js";
+import { createController } from "./controllers/page/create.js";
+import { detailsController } from "./controllers/page/details.js";
+import { homeController } from "./controllers/page/home.js";
+import { myTeamController } from "./controllers/page/myTeams.js";
+import { loginController } from "./controllers/authViews/login.js";
+import { registerController } from "./controllers/authViews/register.js";
+import { attachRender } from "./middlewares/middleware.js";
+import { logoutController } from "./controllers/authViews/logout.js";
+import { showNavigation } from "./middlewares/navigation.js";
+import { teamMembers } from "./middlewares/team.js";
+
+
+page(attachRender);
+page(showNavigation);
+page(teamMembers);
+
+page('/', homeController);
+
+page('/catalog', catalogController);
+
+page('/details/:id',detailsController);
+
+page('/own/teams',myTeamController);
+
+page('/create', createController);
+
+page('/auth/login', loginController);
+
+page('/auth/register', registerController);
+
+page('/auth/logout', logoutController);
+
+
+page.start();
