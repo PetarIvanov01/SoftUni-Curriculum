3136f6752f639f22a5f082e3822a6b606c79a30a
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-19 21:39:18 +0300

    Commit message: "Added controllers and templates for catalog and team creation views"

---
+++ JavaScript-Advanced-Module/Js Applications/07.Exercise Modular Applications/src/controllers/page/create.js
@@ -0,0 +1,46 @@
+import { createTeamReq } from "../../service/api-cals.js";
+import { html } from "../../util.js"
+
+export function createController(ctx) {
+
+    update(null);
+
+    function update(errorMsg) {
+        ctx.render(template(onSubmit, errorMsg));
+    }
+
+    async function onSubmit(event) {
+        try {
+            event.preventDefault();
+            const formData = new FormData(event.target);
+            const { name, logoUrl, description } = Object.fromEntries(formData);
+
+            if (name.trim() == '' || logoUrl.trim() == '' || description.trim() == '') {
+                throw new Error('All fields are required!');
+            }
+            const item = await createTeamReq({ name, logoUrl, description });
+            //TODO validation
+            ctx.page.redirect(`/details/${item._id}`);
+
+        } catch (error) {
+            return update(error.message);
+        }
+
+    }
+}
+
+const template = (onSubmit, error) => html`
+<section id="create">
+<article class="narrow">
+    <header class="pad-med">
+        <h1>New Team</h1>
+    </header>
+    <form id="create-form" @submit=${onSubmit} class="main-form pad-large">
+    ${error ? html`<div class="error">${error}</div>` : ''} 
+        <label>Team name: <input type="text" name="name"></label>
+        <label>Logo URL: <input type="text" name="logoUrl"></label>
+        <label>Description: <textarea name="description"></textarea></label>
+        <input class="action cta" type="submit" value="Create Team">
+    </form>
+</article>
+</section>`
