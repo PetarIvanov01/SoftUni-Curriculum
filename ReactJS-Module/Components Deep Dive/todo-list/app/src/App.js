8adac3b10cf46c9f2ac6d46bc989649c9e6344c5
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-18 15:01:46 +0300

    Structured app layout with components

---
+++ ReactJS-Module/Components Deep Dive/todo-list/app/src/App.js
@@ -0,0 +1,22 @@
+import Footer from "./components/Footer";
+import Main from "./components/Main.js/Main";
+import Navigation from "./components/Navigation";
+
+function App() {
+
+
+
+
+  
+  return (
+    <div>
+      <Navigation />
+
+      <Main />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
