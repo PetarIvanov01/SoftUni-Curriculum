fffa3feae79ef752e2a97f329e2313a49ea4e355
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-20 15:24:34 +0300

    Implement 404 Error Page and Not Authorized Components

---
+++ ReactJS-Module/Workshop-Routing/app/src/components/Errors/404.js
@@ -0,0 +1,9 @@
+export function Error404() {
+
+
+    return (
+        <section id="dashboard">
+            <h2>Resourse not found!</h2>
+        </section>
+    )
+}
---
+++ ReactJS-Module/Workshop-Routing/app/src/components/Errors/NotAuthorize.js
@@ -0,0 +1,8 @@
+export default function NotAuthorized() {
+
+    return (
+        <section id="dashboard">
+            <h2>Not Authorized!</h2>
+        </section>
+    )
+}
