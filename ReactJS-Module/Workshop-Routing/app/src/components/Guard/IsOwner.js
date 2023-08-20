fca364f9648def19a321701e165ec19765354820
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-20 15:26:08 +0300

    This commit introduces the IsOwner component to manage content access based on ownership.

---
+++ ReactJS-Module/Workshop-Routing/app/src/components/Guard/IsOwner.js
@@ -0,0 +1,22 @@
+import { Outlet, useParams } from "react-router-dom";
+import { getDetails } from "../../service/offerService";
+import NotAuthorized from "../Errors/NotAuthorize";
+import { useEffect, useState } from "react";
+
+export default function IsOwner({ user }) {
+
+    const { id } = useParams();
+    const [isOwner, setIfOwner] = useState(false)
+    useEffect(() => {
+
+        getDetails(id)
+            .then(offer => {
+                if (offer._ownerId === user.id) {
+                    setIfOwner(true);
+                }
+            })
+            .catch(error => setIfOwner(false))
+    }, [user])
+
+    return isOwner ? <Outlet /> : <NotAuthorized />
+}
